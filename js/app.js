4e9bafab5a68a7fdb69475bd982cf45239d31527
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-02 14:51:29 -0400

    mostly comments

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 $(function() {
 
+  var question_num = 1;
+
   var questions = [
   {
     question: "When did?",
     option1: "bob",
     option2: "nick"
   }
-
-
   ]
 
+  // gonna need stuff incremented here
   $("form").prepend("<p>" + questions[0].question + "</p>")
 
   // next question function
+  // can delete the form and remake it
 
   var stuff = ["w/e"]
 
