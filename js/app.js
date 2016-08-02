7b2aca1d27f7988591efd4915ca056456a69522c
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-02 14:33:30 -0400

    dynamic input field

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 $(function() {
 
+  $("form").prepend("<input type='text'>")
+
   // form is appened onto the page at start
 $("form").submit(function() {
-    // alert('hi');
+    alert('hi');
 
 
 
