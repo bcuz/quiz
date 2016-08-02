09721a89f70b57d7931330435cf7b73792a32839
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-02 14:34:21 -0400

    comment

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(function() {
 
   $("form").prepend("<input type='text'>")
 
+  // next question function
+
   // form is appened onto the page at start
 $("form").submit(function() {
     alert('hi');
